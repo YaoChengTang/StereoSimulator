2114a9126ede035f2be0010a0e00db8d692bee9c
YaoChengTang <3120215504@bit.edu.cn>
2025-02-28 23:58:02 +0800

    fea(camera): process all videos;
    1. vis by controllable variable;
    2. process all videos;

--- Camera/show_warp_offline.py
+++ Camera/show_warp_offline.py
@@ -69,41 +69,42 @@ def main(args):
         img_depth_zed, img_depth_invalid_zed = img_depth_lownoise, invalid_mask_lownoise
 
         # Visualization
-        vis_scale = 0.25
-        img_L515_resized = cv2.resize(img_L515, (0, 0), fx=vis_scale, fy=vis_scale)
-        depth_colormap = colorize_depth_rgb(img_depth, img_L515, color_type="both")
-        depth_colormap_resized = cv2.resize(depth_colormap, (0, 0), fx=vis_scale, fy=vis_scale)
-        invalid_mask_resized = cv2.resize((np.abs(img_depth)<np.finfo(np.float32).eps)*255., (0, 0), fx=vis_scale, fy=vis_scale).astype(np.uint8)
-        vis_L515 = stack_images([img_L515_resized, depth_colormap_resized, invalid_mask_resized], 
-                                orientation='vertical')
+        if args.vis:
+            vis_scale = 0.25
+            img_L515_resized = cv2.resize(img_L515, (0, 0), fx=vis_scale, fy=vis_scale)
+            depth_colormap = colorize_depth_rgb(img_depth, img_L515, color_type="both")
+            depth_colormap_resized = cv2.resize(depth_colormap, (0, 0), fx=vis_scale, fy=vis_scale)
+            invalid_mask_resized = cv2.resize((np.abs(img_depth)<np.finfo(np.float32).eps)*255., (0, 0), fx=vis_scale, fy=vis_scale).astype(np.uint8)
+            vis_L515 = stack_images([img_L515_resized, depth_colormap_resized, invalid_mask_resized], 
+                                    orientation='vertical')
 
-        img_ZED_resized = cv2.resize(img_ZED, (0, 0), fx=vis_scale*2/3, fy=vis_scale*2/3)
-        depth_colormap = colorize_depth_rgb(img_depth_remap, img_ZED, color_type="both")
-        depth_colormap_resized = cv2.resize(depth_colormap, (0, 0), fx=vis_scale*2/3, fy=vis_scale*2/3)
-        invalid_mask_remap_resized = cv2.resize(invalid_mask_remap*255., (0, 0), fx=vis_scale*2/3, fy=vis_scale*2/3).astype(np.uint8)
-        vis_ZED_remap = stack_images([img_ZED_resized, depth_colormap_resized, invalid_mask_remap_resized], 
-                                     orientation='vertical')
+            img_ZED_resized = cv2.resize(img_ZED, (0, 0), fx=vis_scale*2/3, fy=vis_scale*2/3)
+            depth_colormap = colorize_depth_rgb(img_depth_remap, img_ZED, color_type="both")
+            depth_colormap_resized = cv2.resize(depth_colormap, (0, 0), fx=vis_scale*2/3, fy=vis_scale*2/3)
+            invalid_mask_remap_resized = cv2.resize(invalid_mask_remap*255., (0, 0), fx=vis_scale*2/3, fy=vis_scale*2/3).astype(np.uint8)
+            vis_ZED_remap = stack_images([img_ZED_resized, depth_colormap_resized, invalid_mask_remap_resized], 
+                                         orientation='vertical')
 
-        depth_colormap_repair = colorize_depth_rgb(img_depth_remap_repair, img_ZED, color_type="both")
-        depth_colormap_repair_resized = cv2.resize(depth_colormap_repair, (0, 0), fx=vis_scale*2/3, fy=vis_scale*2/3)
-        invalid_mask_repair_resized = cv2.resize(invalid_mask_remap_repair*255., (0, 0), fx=vis_scale*2/3, fy=vis_scale*2/3).astype(np.uint8)
-        vis_ZED_repair = stack_images([None, depth_colormap_repair_resized, invalid_mask_repair_resized], 
-                                      orientation='vertical', placeholder_size=img_ZED_resized.shape[0])
+            depth_colormap_repair = colorize_depth_rgb(img_depth_remap_repair, img_ZED, color_type="both")
+            depth_colormap_repair_resized = cv2.resize(depth_colormap_repair, (0, 0), fx=vis_scale*2/3, fy=vis_scale*2/3)
+            invalid_mask_repair_resized = cv2.resize(invalid_mask_remap_repair*255., (0, 0), fx=vis_scale*2/3, fy=vis_scale*2/3).astype(np.uint8)
+            vis_ZED_repair = stack_images([None, depth_colormap_repair_resized, invalid_mask_repair_resized], 
+                                          orientation='vertical', placeholder_size=img_ZED_resized.shape[0])
 
-        depth_colormap_ivlarea = colorize_depth_rgb(img_depth_ivlarea, img_ZED, color_type="both")
-        depth_colormap_ivlarea_resized = cv2.resize(depth_colormap_ivlarea, (0, 0), fx=vis_scale*2/3, fy=vis_scale*2/3)
-        invalid_mask_ivlarea_resized = cv2.resize(invalid_mask_ivlarea*255., (0, 0), fx=vis_scale*2/3, fy=vis_scale*2/3).astype(np.uint8)
-        vis_ZED_ivlarea = stack_images([None, depth_colormap_ivlarea_resized, invalid_mask_ivlarea_resized], 
-                                       orientation='vertical', placeholder_size=img_ZED_resized.shape[0])
+            depth_colormap_ivlarea = colorize_depth_rgb(img_depth_ivlarea, img_ZED, color_type="both")
+            depth_colormap_ivlarea_resized = cv2.resize(depth_colormap_ivlarea, (0, 0), fx=vis_scale*2/3, fy=vis_scale*2/3)
+            invalid_mask_ivlarea_resized = cv2.resize(invalid_mask_ivlarea*255., (0, 0), fx=vis_scale*2/3, fy=vis_scale*2/3).astype(np.uint8)
+            vis_ZED_ivlarea = stack_images([None, depth_colormap_ivlarea_resized, invalid_mask_ivlarea_resized], 
+                                           orientation='vertical', placeholder_size=img_ZED_resized.shape[0])
 
-        vis_img = stack_images([vis_L515, vis_ZED_remap, vis_ZED_repair, vis_ZED_ivlarea], orientation='horizontal')
+            vis_img = stack_images([vis_L515, vis_ZED_remap, vis_ZED_repair, vis_ZED_ivlarea], orientation='horizontal')
 
-        # print("-"*10, invalid_mask_remap.shape, img_depth_remap.shape)
+            # print("-"*10, invalid_mask_remap.shape, img_depth_remap.shape)
 
+            cv2.imshow(f"{scenename}-{frame_idx}", vis_img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
-        cv2.imshow(f"{scenename}-{frame_idx}", vis_img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
 
         # Save the final depth map for ZED left camera
         data = {
@@ -112,6 +113,7 @@ def main(args):
         }
         save_data(args, data, frame_idx)
 
+
         # Save point cloud with RGB
         if args.sv_ply:
             save_ply(img_L515, img_depth, depth_scale, K_L515, 
@@ -119,7 +121,7 @@ def main(args):
             save_ply(img_ZED, img_depth_remap_repair, depth_scale, K_ZED, 
                      "ZED.ply", args=args, frame_idx=frame_idx)
 
-        break
+        # break
 
 
 
@@ -127,13 +129,25 @@ if __name__ == "__main__":
     # Parse command line arguments
     parser = argparse.ArgumentParser(description="RealSense Stream")
     parser.add_argument('--root', type=str, default="./Dataset", help='Dataset root')
-    parser.add_argument('--scene_name', type=str, default=datetime.now().strftime('%Y%m%d_%H%M%S'), help='Scene name for saving images')
+    parser.add_argument('--scene_name', type=str, default="", help='Scene name for saving images')
     parser.add_argument('--vis_debug', action='store_true', help='Save visualization for debug')
     parser.add_argument('--sv_ply', action='store_true', help='Save point cloud with RGB')
+    parser.add_argument('--vis', action='store_true', help='Visualize intermediate results')
     args = parser.parse_args()
 
     args.root = "./Dataset"
     # args.scene_name = "demo-20250203_211254"
-    args.scene_name = "demo-20250205_001631"
+    # args.scene_name = "demo-20250205_001631"
+    # args.scene_name = "20250224_191550"
 
-    main(args)
+    if len(args.scene_name)>0:
+        main(args)
+    else:
+        for scene_name in os.listdir(args.root):
+            if scene_name!="calib" and os.path.exists( os.path.join(args.root,scene_name,"L515_calib.yaml") ):
+                args.scene_name = scene_name
+                print(f"Processing {scene_name}")
+                main(args)
+                # break
+
+    
